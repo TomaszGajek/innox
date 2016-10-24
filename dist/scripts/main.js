415f033efe6a199c5326ffe97c63df39e4ce46dd
Tomasz Gajek <tomaszgajek@poczta.onet.pl>
2016-10-24 16:02:50 +0200

    thumbnail filter v. 1.2

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,1 +1,9 @@
-var x = 'tomek';
+$(document).ready(function () {
+
+  var listItem = $('.works__list__item');
+
+  listItem.on('click', function () {
+    listItem.removeClass('works__list__item--active');
+    $(this).toggleClass('works__list__item--active');
+  });
+});
