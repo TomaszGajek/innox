24f96cae0d1e7a42615830cfa87f27bcf6e5741d
Tomasz Gajek <tomaszgajek@poczta.onet.pl>
2016-10-24 16:43:05 +0200

    section works finished

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -2,8 +2,20 @@ $(document).ready(function () {
 
   var listItem = $('.works__list__item');
 
+  thumb = $('.thumbnail');
+
   listItem.on('click', function () {
+
+    var filter = $(this).text();
+
     listItem.removeClass('works__list__item--active');
     $(this).toggleClass('works__list__item--active');
+
+    if (filter == 'All') {
+      thumb.show();
+    } else {
+      thumb.hide();
+      thumb.filter("." + filter).show();
+    }
   });
 });
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,15 +2,25 @@ $(document).ready(function(){
 
   var listItem = $('.works__list__item');
 
+      thumb = $('.thumbnail');
 
   listItem.on('click',function(){
+
+    var filter = $(this).text();
+
     listItem.removeClass('works__list__item--active');
     $(this).toggleClass('works__list__item--active');
 
 
+
+    if(filter == 'All'){
+      thumb.show();
+    } else {
+      thumb.hide();
+      thumb.filter("."+filter).show();
+    }
+
   });
-
-
 
 
 
