cdfae7aef0cc5db6a65546288257b63b19112217
Tomasz Gajek <tomaszgajek@poczta.onet.pl>
2016-10-24 21:52:04 +0200

    clients section final

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,8 +22,28 @@ $(document).ready(function(){
 
   });
 
-
-
+  $("#owl-clients").owlCarousel({
+    items:4,
+    loop:true,
+    margin: 10,
+    responsiveClass:true,
+    autoplay:true,
+    autoplayTimeout:2500,
+    responsive:{
+        0:{
+            items:1,
+            nav: true
+        },
+        600:{
+            items:3,
+            nav: true
+        },
+        1000:{
+            items:4,
+            nav: true
+        }
+    }
+  });
 
 
 
